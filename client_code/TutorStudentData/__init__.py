4a0024aab2c18915ce1dc5683fd0309d85bbfe5d
mindspireappdev@gmail.com <mindspireappdev@gmail.com>
2023-12-14 20:58:28 +0000

    Edited settings and TutorStudentData; Deleted TutorDashboard

--- client_code/TutorStudentData/__init__.py
+++ client_code/TutorStudentData/__init__.py
@@ -9,8 +9,14 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class TutorStudentData(TutorStudentDataTemplate):
-  def __init__(self, **properties):
+  def __init__(self, user = 'karl.zipple@gmail.com', **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.user = user
+    self.rtTitle.content = '## Welcome back, ' + app_tables.user.get(userID=user)['firstName'] + '!'
+    item_list = ['']
+    for row in app_tables.user.search(tutorID = self.user):
+      item_list.append((row['firstName'] + ' ' + row['lastName'], row))
+    self.ddStudent.items = item_list
 
     # Any code you write here will run before the form opens.
